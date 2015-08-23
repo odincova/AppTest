783d7e4bcb58f83868c90ee8b6047e01aac12caa
yodincova <odincova1024@gmail.com>
2015-08-23 17:25:35 +0300

    Initial Commit

---
+++ DrinkMixer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrinkMixer
+//
+//  Created by Juliya on 23.08.15.
+//  Copyright (c) 2015 Juliya Odincova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DrinkMixer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DrinkMixer
+//
+//  Created by Juliya on 23.08.15.
+//  Copyright (c) 2015 Juliya Odincova. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
